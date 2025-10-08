010cbd5ffd6e27a7df1ba0d9371a89ec6798f2ed
Butanol <51089545+Butanol@users.noreply.github.com>
2025-10-08 20:24:46 +0800

    Add IC model
    Lacked data model for IC.
    
    Added an abstraction of IC model.

---
+++ src/main/java/seedu/address/model/person/IC.java
@@ -0,0 +1,28 @@
+package seedu.address.model.person;
+
+public class IC {
+    public final String ic;
+
+    public IC(String ic) {
+        this.ic = ic;
+    }
+
+    @Override
+    public String toString() {
+        return ic;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof IC)) {
+            return false;
+        }
+
+        IC otherIC = (IC) other;
+        return ic.equals(otherIC.ic);
+    }
+}
