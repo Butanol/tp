dcb9ef2f8b05d503b488b88cf37773f316f55a59
Butanol <51089545+Butanol@users.noreply.github.com>
2025-10-16 23:22:38 +0800

    Edit Grammar of prompt

--- src/main/java/seedu/noknock/logic/commands/EditPatientCommand.java
+++ src/main/java/seedu/noknock/logic/commands/EditPatientCommand.java
@@ -35,7 +35,7 @@ public class EditPatientCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the patient identified "
         + "by the index number used in the displayed patient list. "
         + "Existing values will be overwritten by the input values.\n"
-        + "Parameters: INDEX (must be a positive integer) "
+        + "Parameters: PATIENT INDEX (must be a positive integer) "
         + "[" + PREFIX_NAME + "NAME] "
         + "[" + PREFIX_IC + "IC]"
         + "[" + PREFIX_WARD + "WARD"
