085facb41c1e836523d22d8341c83913278eecba
Butanol <51089545+Butanol@users.noreply.github.com>
2025-10-29 15:25:58 +0800

    Test DeletePatientCommandParser
    DeletePatientCommandParser did not have test cases.
    
    Tested DeletePatientCommandParser class.

---
+++ src/test/java/seedu/noknock/logic/parser/DeletePatientCommandParserTest.java
@@ -0,0 +1,24 @@
+package seedu.noknock.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.noknock.logic.commands.DeleteCommand;
+import seedu.noknock.logic.commands.DeletePatientCommand;
+
+import static seedu.noknock.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.noknock.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.noknock.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.noknock.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+public class DeletePatientCommandParserTest {
+    DeletePatientCommandParser parser = new DeletePatientCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeletePatientCommand() {
+        assertParseSuccess(parser, "1", new DeletePatientCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePatientCommand.MESSAGE_USAGE));
+    }
+}
